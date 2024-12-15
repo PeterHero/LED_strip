68e304b33a7a620796153daf1c6b26110966eef0
Petr Hrdina <phrdina@proton.me>
2024-12-15 10:27:57 +0100

    Improve script and simplify src code

--- LED_strip.ino
+++ LED_strip.ino
@@ -1,23 +1,27 @@
 #include "src/strip_controller.h"
-#include "src/bluetooth.h"
+#include <SoftwareSerial.h>
 
 StripController strip;
 CRGB colors[] = {CRGB::Red, CRGB::Green, CRGB::Blue};
 size_t colors_size = sizeof(colors) / sizeof(colors[0]);
 constexpr size_t TIME = 500;
 
-Bluetooth bt;
+constexpr byte rxPin = 3;
+constexpr byte txPin = 2;
+SoftwareSerial bluetooth(rxPin, txPin);
 
 void setup() {
   strip.setup();
   strip.simple(CRGB::Blue, 10);
 
-  bt.setup();
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  bluetooth.begin(9600);
 }
 
 void loop() {
-  String message;
-  if (bt.read(message)) {
+  if (bluetooth.available() > 0) {
+    bluetooth.readString();
     strip.simple(CRGB::Green, 10);
   }
   // strip.flash(colors, colors_size, TIME);
--- ledstrip.sh
+++ ledstrip.sh
@@ -6,10 +6,19 @@ MAC="98:D3:51:F9:28:75"
 # connect pair bluetooth device
 
 # check if paired
-bluetoothctl info $MAC | grep "Paired"
+if ! bluetoothctl info $MAC | grep "Paired: yes" >/dev/null 2>&1 ; then
+    # connect to bluetooth
+    exit 1
+fi
 
 # bind device
-sudo rfcomm bind 0 $MAC
-sudo chmod o+rw /dev/rfcomm0
+
+if ! ls /dev/rfcomm0 >/dev/null 2>&1 ; then
+    sudo rfcomm bind 0 $MAC
+    sudo chmod o+rw /dev/rfcomm0
+fi
+
+echo "Connected"
+echo "start" > /dev/rfcomm0
 
 # write commands to the device
